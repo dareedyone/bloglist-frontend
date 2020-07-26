1e3f6139d7ce82e726ce78b04025748f94200892
Ridwan Ijadunola <dareedyone@gmail.com>
2020-07-26 18:57:35 +0100

    5.7* Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+	const [view, setView] = useState(false);
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 10,
+		border: "solid",
+		borderWidth: 1,
+		marginBottom: 5,
+	};
 
-export default Blog
+	return (
+		<div style={blogStyle}>
+			<p>
+				{blog.title} {blog.author}
+				<button onClick={() => setView(!view)}>
+					<small>{view ? "hide" : "view"}</small>
+				</button>
+			</p>
+			{view && (
+				<>
+					<p>{blog.url}</p>
+					<p>
+						likes {blog.likes}{" "}
+						<button>
+							<small>like</small>
+						</button>
+					</p>
+				</>
+			)}
+		</div>
+	);
+};
+
+export default Blog;
