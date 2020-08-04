be71703d806fa62457e29c720a0521ed95ecfb44
Ridwan Ijadunola <dareedyone@gmail.com>
2020-08-04 15:02:51 +0100

    5.18: bloglist end to end testing, step2

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,15 @@
 describe("Blog app", function () {
 	beforeEach(function () {
 		cy.request("POST", "http://localhost:3001/api/testing/reset");
+		cy.request("POST", "http://localhost:3001/api/users", {
+			username: "strayen",
+			password: "test1234",
+		});
+
+		// .then((res) => {
+		//     localStorage.setItem("loggedUser", JSON.stringify(res.body));
+
+		// });
 		cy.visit("http://localhost:3000");
 	});
 
@@ -9,5 +18,23 @@ describe("Blog app", function () {
 		cy.contains("username");
 		cy.contains("password");
 		cy.contains("login");
+	});
+
+	describe("Login", function () {
+		it("succeeds with correct credentials", function () {
+			cy.get("input[name='username']").type("strayen");
+			cy.get("input[name='password']").type("test1234");
+			cy.contains("login").click();
+			cy.contains("logged in");
+		});
+
+		it("fails with wrong credentials", function () {
+			cy.get("input[name='username']").type("strayen");
+			cy.get("input[name='password']").type("wrong");
+			cy.contains("login").click();
+			cy.get(".message")
+				.should("contain", "Wrong username or password !")
+				.and("have.css", "color", "rgb(255, 0, 0)");
+		});
 	});
 });
