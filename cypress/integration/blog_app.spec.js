6bfe78989ebae0dd6899cf2cc1644e26589618e4
Ridwan Ijadunola <dareedyone@gmail.com>
2020-08-04 15:54:19 +0100

    5.20: bloglist end to end testing, step4

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -53,5 +53,25 @@ describe("Blog app", function () {
 			cy.contains("create").click();
 			cy.contains("A new blog - A cypress title by Tester Man added !");
 		});
+
+		describe.only("When there is a blog", function () {
+			beforeEach(function () {
+				cy.contains("new note").click();
+				cy.get("input[name='title']").type("A cypress title");
+				cy.get("input[name='author']").type("Tester Man");
+				cy.get("input[name='url']").type("example.com");
+				cy.contains("create").click();
+				cy.contains("A new blog - A cypress title by Tester Man added !");
+			});
+
+			it("user can like a blog.", function () {
+				cy.contains("A cypress title Tester Man").find("button").click();
+				cy.contains("A cypress title Tester Man")
+					.parent()
+					.find(".like_btn")
+					.click();
+				cy.contains("A cypress title Tester Man").parent().contains("likes 1");
+			});
+		});
 	});
 });
